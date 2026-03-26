dc80a6ec392e9de01ed5b570f789c56bf1391a86
jeiwyy <jeiwy1333@gmail.com>
2026-03-26 18:26:18 +0500

    add lab_5_1

--- lab_5_1.pl
+++ lab_5_1.pl
@@ -1,12 +1,12 @@
 % Составить программу вычисления наибольшего общего делителя двух натуральных чисел.
-:- initialization(start).
+:- initialization(main).
 gcd(X, 0, X) :- X > 0, !.
 gcd(X, Y, D) :-
     Y > 0,
     R is X mod Y,
     gcd(Y, R, D).
 
-start :-
+main :-
     write('Введите первое число: '),
     read(A),
     write('Введите второе число'),
